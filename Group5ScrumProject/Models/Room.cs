94b403ff559b27b50ea421be1a9fb2dec0189ce2
hsahlberg <hannah.sahlberg@gmail.com>
2014-02-04 15:56:13 +0100

    RoomClass

---
+++ Group5ScrumProject/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group5ScrumProject.Models
+{
+    public class Room
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
+        public Room(int id)
+        {
+            RoomId = id;
+            RoomName = db.tbRooms
+                .Where(i => i.iRoomId == RoomId)
+                .FirstOrDefault().sRoomName;
+        }
+
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+    }
+}
