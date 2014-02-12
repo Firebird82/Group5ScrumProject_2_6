96ee9b9b2fab0e9b5edc202316d5e503eba3dfa2
Firebird82 <david.sebela@gmail.com>
2014-02-12 13:01:03 +0100

    BookningsLista till Index

--- Group5ScrumProject/Models/Room.cs
+++ Group5ScrumProject/Models/Room.cs
@@ -21,5 +21,27 @@ namespace Group5ScrumProject.Models
         public string RoomName { get; set; }
         public int Chairs { get; set; }
         public string RoomDescription { get; set; }
+
+        public List<BookingInfo> BookingList(DateTime day)
+        {
+            List<BookingInfo> bookingList = new List<BookingInfo>();
+            List<TimeSpan> timeList = new List<TimeSpan>();
+
+            timeList.Add(new TimeSpan(9, 0, 0));
+            timeList.Add(new TimeSpan(10, 0, 0));
+            timeList.Add(new TimeSpan(11, 0, 0));
+            timeList.Add(new TimeSpan(12, 0, 0));
+            timeList.Add(new TimeSpan(13, 0, 0));
+            timeList.Add(new TimeSpan(14, 0, 0));
+            timeList.Add(new TimeSpan(15, 0, 0));
+            timeList.Add(new TimeSpan(16, 0, 0));
+
+            foreach (TimeSpan time in timeList)
+            {
+                bookingList.Add(new BookingInfo(RoomId, time, day));
+            }
+            
+            return bookingList;
+        }
     }
 }
