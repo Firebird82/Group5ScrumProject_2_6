4c9c2b9f0a3a68bd34cb533808cf99189fc4c99d
Firebird82 <david.sebela@gmail.com>
2014-02-12 15:06:57 +0100

    Index LIst

--- Group5ScrumProject/Models/BookingInfo.cs
+++ Group5ScrumProject/Models/BookingInfo.cs
@@ -19,16 +19,18 @@ namespace Group5ScrumProject.Models
             {
 
                 BookedById = booking.iUserId.ToString(); //Hämta användarens namn och klass David Sebela - SHAN13
-                RoomId = booking.iRumId;  
-                Time = booking.dtTimeStart.ToString();
+                RoomId = roomId; 
+                TimeStart = booking.dtTimeStart.ToString();
+                TimeEnd = time.Add(TimeSpan.FromHours(1)).ToString();
                 IsBooked = "Redan bokad";
                 BookingDay = booking.dtDateDay.ToString();   
             }
             else
             {
                 BookedById = "";
-                RoomId = 0;
-                Time = time.ToString();
+                RoomId = roomId;
+                TimeStart = time.ToString();
+                TimeEnd = time.Add(TimeSpan.FromHours(1)).ToString();
                 IsBooked = "Boka";
                 BookingDay = "";
             }
@@ -36,7 +38,8 @@ namespace Group5ScrumProject.Models
 
         public string BookedById { get; set; }
         public int RoomId { get; set; }
-        public string Time { get; set; }
+        public string TimeStart { get; set; }
+        public string TimeEnd { get; set; }
         public string IsBooked { get; set; }
         public string BookingDay { get; set; }
     }
