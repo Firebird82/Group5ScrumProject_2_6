efa31b46e846e78a4146d0fc54c9f7656ca6225e
Firebird82 <david.sebela@gmail.com>
2014-02-12 19:36:42 +0100

    Index list WORKING :)

--- Group5ScrumProject/Models/BookingInfo.cs
+++ Group5ScrumProject/Models/BookingInfo.cs
@@ -9,44 +9,22 @@ namespace Group5ScrumProject.Models
     {
         DataClasses1DataContext db = new DataClasses1DataContext();
 
-        public BookingInfo(int roomId, TimeSpan time, DateTime day)
+        public BookingInfo(string roomId, string bookedBy, string timeStart, string timePlusOneHour, string isBooked, string bookingDay)
         {
-            tbBooking booking = (from f in db.tbBookings
-                                where f.iRumId == roomId && day == f.dtDateDay && time == f.dtTimeStart
-                                select f).FirstOrDefault();
+            RoomId = roomId;
 
-            if (booking != null)
-            {
-                string userName = (from f in db.tbUsers
-                                   where f.iUserId == booking.iUserId
-                                   select f.sUserName).FirstOrDefault();
-
-                string userClass = (from f in db.tbUsers
-                                   where f.iUserId == booking.iUserId
-                                   select f.sClass).FirstOrDefault();
-
-                BookedBy = userName + " - " + userClass;
-                RoomId = roomId.ToString();
-                TimeStart = booking.dtTimeStart.ToString();
-                TimeEnd = time.Add(TimeSpan.FromHours(1)).ToString();
-                IsBooked = "Redan bokad";
-                BookingDay = day.ToString("yyyy/MM/dd");   
-            }
-            else
-            {
-                BookedBy = "";
-                RoomId = roomId.ToString();
-                TimeStart = time.ToString();
-                TimeEnd = time.Add(TimeSpan.FromHours(1)).ToString();
-                IsBooked = "Boka";
-                BookingDay = day.ToString("yyyy/MM/dd"); 
-            }
+            BookedBy = bookedBy;
+            RoomId = roomId;
+            TimeStart = timeStart;
+            TimePlusOneHour = timePlusOneHour;
+            IsBooked = isBooked;
+            BookingDay = bookingDay;
         }
 
         public string BookedBy { get; set; }
         public string RoomId { get; set; }
         public string TimeStart { get; set; }
-        public string TimeEnd { get; set; }
+        public string TimePlusOneHour { get; set; }
         public string IsBooked { get; set; }
         public string BookingDay { get; set; }
     }
--- Group5ScrumProject/Models/Room.cs
+++ Group5ScrumProject/Models/Room.cs
@@ -8,9 +8,11 @@ namespace Group5ScrumProject.Models
     public class Room
     {
         DataClasses1DataContext db = new DataClasses1DataContext();
+        string roomIdNr = "";
 
         public Room(tbRoom room)
         {
+            roomIdNr = room.iRoomId.ToString();
             RoomId = room.iRoomId;
             RoomName = room.sRoomName;
             Chairs = room.iRoomChairs;
@@ -36,9 +38,66 @@ namespace Group5ScrumProject.Models
             timeList.Add(new TimeSpan(15, 0, 0));
             timeList.Add(new TimeSpan(16, 0, 0));
 
+            for (int i = 0; i < timeList.Count-1; i++)
+            {
+                tbBooking booking = (from f in db.tbBookings
+                                     where f.iRumId == RoomId && day == f.dtDateDay && f.dtTimeStart == timeList[i]
+                                     select f).FirstOrDefault();
+
+                if (booking != null)
+                {
+                    TimeSpan startTime = booking.dtTimeStart;
+                    TimeSpan endTime = booking.dtTimeEnd;
+
+                    int bookingTimeDifference = endTime.Hours - startTime.Hours;
+
+                    string userName = (from f in db.tbUsers
+                                       where f.iUserId == booking.iUserId
+                                       select f.sUserName).FirstOrDefault();
+
+                    string userClass = (from f in db.tbUsers
+                                        where f.iUserId == booking.iUserId
+                                        select f.sClass).FirstOrDefault();
+
+                    string bookedBy = userName + " - " + userClass;
+                    string roomId = roomIdNr.ToString();
+                    string isBooked = "Redan bokad";
+                    string bookingDay = day.ToString("yyyy/MM/dd");
+
+                    for (int x = 0; x < bookingTimeDifference; x++)
+                    {
+                        if (x > 0)
+                        {
+                            startTime = startTime.Add(TimeSpan.FromHours(1));    
+                        }
+                        
+                        string timeStart = (startTime.ToString()) ;
+                        string timePlusOneHour = startTime.Add(TimeSpan.FromHours(1)).ToString();
+                     
+                        bookingList.Add(new BookingInfo(roomId, bookedBy, timeStart, timePlusOneHour, isBooked, bookingDay));
+
+                        if (x < bookingTimeDifference-1)
+                        {
+                            i++;   
+                        }   
+                    }
+                }
+                else
+                {
+                    string bookedBy = "";
+                    string roomId = roomIdNr.ToString();
+                    string timeStart = timeList[i].ToString();
+                    string timePlusOneHour = timeList[i].Add(TimeSpan.FromHours(1)).ToString();
+                    string isBooked = "Boka";
+                    string bookingDay = day.ToString("yyyy/MM/dd");
+
+                    bookingList.Add(new BookingInfo(roomId, bookedBy, timeStart, timePlusOneHour, isBooked, bookingDay));
+                }     
+            }
+
             foreach (TimeSpan time in timeList)
             {
-                bookingList.Add(new BookingInfo(RoomId, time, day));
+            
             }
             
             return bookingList;
